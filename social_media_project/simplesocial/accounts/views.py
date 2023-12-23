98c8327e47f152c1ecbec4e1366db91446e36a57
virenn <virenn9999@gmail.com>
2023-12-23 19:53:50 +0530

    Accounts templates completed

--- social_media_project/simplesocial/accounts/views.py
+++ social_media_project/simplesocial/accounts/views.py
@@ -5,4 +5,7 @@ from django.views.generic import CreateView
 from . import forms
 # Create your views here.
 class SignUp(CreateView):
-    pass
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
+
--- social_media_project/simplesocial/simplesocial/settings.py
+++ social_media_project/simplesocial/simplesocial/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'bootstrap5',
+    'accounts',
 ]
 
 MIDDLEWARE = [
